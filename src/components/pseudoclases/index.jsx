dbcdc47ce68ae6af27e173c46d5281829e802f63
Christian <c.espinoza.code@gmail.com>
2023-06-20 14:44:03 -0500

    pseudoclases component created

---
+++ src/components/pseudoclases/index.jsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+function PseudoClases() {
+  return (
+    <div>
+      <a href="/">Hello</a>
+      <p className='world'>World</p>
+      <p>World</p>
+      <div className='container'>
+        <div>Hello</div>
+        <div>Hello</div>
+        <div>Hello</div>
+      </div>
+    </div>
+  )
+}
+
+export { PseudoClases }
