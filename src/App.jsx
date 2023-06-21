39c75c93c684d85a9fa1a9068be3b4938dd95812
Christian <c.espinoza.code@gmail.com>
2023-06-21 10:31:07 -0500

    Scale component styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
-import { PseudoClases } from './components/pseudoclases';
-import { PseudoElements } from './components/pseudoelements';
 import { Translate } from './components/Transform/Translate';
+import { Scale } from './components/Transform/Scale';
+// import { PseudoClases } from './components/pseudoclases';
+// import { PseudoElements } from './components/pseudoelements';
 
 function App() {
   return (
     <>
-      {/* <PseudoClases />
-      <PseudoElements /> */}
       <p>Translate</p>
       <Translate />
+      <p>Scale</p>
+      <Scale />
     </>
   );
 }
