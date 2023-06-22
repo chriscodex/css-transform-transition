0853f956f548d8b0d23a948a3126e4b77c51a219
Christian <c.espinoza.code@gmail.com>
2023-06-22 17:05:08 -0500

    Transition page structure creadte

---
+++ src/pages/Transition/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+import { Duration } from '../../components/Transition/Duration'
+
+function Transition() {
+  return (
+    <div>
+      <Duration />
+    </div>
+  )
+}
+
+export { Transition }
