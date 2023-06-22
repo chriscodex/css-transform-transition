bd1dd682e4019ed7a41264e0ddc1ae00a123dd60
Christian <c.espinoza.code@gmail.com>
2023-06-22 12:25:36 -0500

    style perspective component styles added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { Translate } from './components/Transform/Translate';
 import { Scale } from './components/Transform/Scale';
 import { Skew } from './components/Transform/Skew';
+import { Rotate } from './components/Transform/Rotate';
+import { StylePerspective } from './components/Transform/StylePerspective';
 // import { PseudoClases } from './components/pseudoclases';
 // import { PseudoElements } from './components/pseudoelements';
 
@@ -14,6 +16,10 @@ function App() {
       <Scale />
       <p>Skew</p>
       <Skew />
+      <p>Rotate & Origin</p>
+      <Rotate />
+      <p>Style & Perspective</p>
+      <StylePerspective />
     </>
   );
 }
