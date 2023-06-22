9738fd8d08b8f98f561d81fd8b46a0de5f15bac2
Christian <c.espinoza.code@gmail.com>
2023-06-22 12:25:28 -0500

    style perspective component structure created

---
+++ src/components/Transform/StylePerspective/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function StylePerspective() {
+  return (
+    <div className='father-container'>
+      <div className="style-container">
+        <div className="style-item"></div>
+      </div>
+    </div>
+  );
+}
+
+export { StylePerspective };
