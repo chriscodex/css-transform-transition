10e260b176f20b4058dbc03ee7006ccc6f755de7
Christian <c.espinoza.code@gmail.com>
2023-06-23 17:06:42 -0500

    duration example for transition duration added

---
+++ src/components/Transition/Duration/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function Duration() {
+  return (
+    <div className='duration-container'>
+      <div className="duration-box box1"></div>
+      <div className="duration-box box2"></div>
+      <div className="duration-box box3"></div>
+    </div>
+  )
+}
+
+export { Duration }
