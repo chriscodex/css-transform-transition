6fe1b614b98ae08afbeed6e5c0a58cda7141452e
Christian <c.espinoza.code@gmail.com>
2023-06-24 06:22:50 -0500

    Movimiento reducido component created

---
+++ src/components/Optimization/MovimientoReducido/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function MovimientoReducido() {
+  return (
+    <div className='movimiento-container'>
+      <div className='face front'></div>
+      <div className='face back'></div>
+    </div>
+  )
+}
+
+export { MovimientoReducido }
--- src/pages/Optimization/index.jsx
+++ src/pages/Optimization/index.jsx
@@ -1,10 +1,13 @@
 import { WillChange } from '../../components/Optimization/WillChange'
+import { MovimientoReducido } from '../../components/Optimization/MovimientoReducido'
 
 function Optimization() {
   return (
     <div>
       <p>Aceleración de Hardware y la propiedad will change</p>
       <WillChange />
+      <p>Preferencias de Movimiento Reducido</p>
+      <MovimientoReducido />
     </div>
   )
 }
