bd64f935344f49437a7505ce052b9fa2ef33ef79
Christian <c.espinoza.code@gmail.com>
2023-06-23 23:07:48 -0500

    Timing&Delay component styles added

--- src/components/Transition/TimingDelay/index.jsx
+++ src/components/Transition/TimingDelay/index.jsx
@@ -2,7 +2,14 @@ import './styles.css'
 
 function TimingDelay() {
   return (
-    <div></div>
+    <div className='timing-container'>
+      <div className='timing-circle one'></div>
+      <div className='timing-circle two'></div>
+      <div className='timing-circle three'></div>
+      <div className='timing-circle four'></div>
+      <div className='timing-circle five'></div>
+      <div className='timing-circle six'></div>
+    </div>
   )
 }
 
